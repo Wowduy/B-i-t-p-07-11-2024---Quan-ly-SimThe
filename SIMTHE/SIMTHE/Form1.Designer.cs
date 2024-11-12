7d03cce3ee1a1e3d46e31bc7bb9e29686108c514
Wowduy <161957830+Wowduy@users.noreply.github.com>
2024-11-12 17:16:55 +0700

    Add files via upload

---
+++ SIMTHE/SIMTHE/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SIMTHE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewSim = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewSim
+            // 
+            this.dataGridViewSim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSim.Location = new System.Drawing.Point(71, 61);
+            this.dataGridViewSim.Name = "dataGridViewSim";
+            this.dataGridViewSim.RowTemplate.Height = 25;
+            this.dataGridViewSim.Size = new System.Drawing.Size(669, 180);
+            this.dataGridViewSim.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(272, 293);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(201, 63);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "LoadData";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 490);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dataGridViewSim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSim)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewSim;
+        private Button btnLoadData;
+    }
+}
---
+++ SIMTHE/SIMTHE/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace SIMTHE
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=LAPTOP-0GVJC0P1\\SQLEXPRESS;Initial Catalog=sqlSIMTHE;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Sim ORDER BY NgayKichHoat ASC"; // Truy vấn SQL để lấy dữ liệu
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridViewSim.DataSource = dataTable; // Gán dữ liệu cho DataGridView
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+        }
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
